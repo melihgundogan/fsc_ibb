98cd3b1dc830ecd8d4de22efdc5c1c1dcb9c5ccb
Gürkan Fikret Günak <gurkanfikretgunak@gmail.com>
2022-02-09 06:04:28 +0300

    fix

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,18 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:fsc_ibb/views/splash/widgets/app_bar_actions.dart';
 import 'package:stacked/stacked.dart';
 import 'splash_view_model.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SplashView extends StatelessWidget {
+  const SplashView({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.reactive(
       builder: (BuildContext context, SplashViewModel viewModel, Widget _) {
         return Scaffold(
-          appBar: AppBar(title: Text(AppLocalizations.of(context).title)),
-          body: Center(
-            child: Text('Splash View'),
+          /// Side slider menu came to [Drawer] its was Custom.
+          drawer: const Drawer(),
+
+          /// Top App Bar will be modify [AppBar]
+          appBar: AppBar(
+            // ignore: prefer_const_literals_to_create_immutables
+            actions: [
+              const AppBarActions(),
+            ],
+
+            /// App Title String Method - > [AppLocalizations]
+            title: Text(
+              AppLocalizations.of(context).title,
+              style: const TextStyle(color: Colors.black),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leadingWidth: 56,
+            leading: Row(
+              children: [
+                Container(
+                  width: 28,
+                  height: 30,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 28,
+                  height: 30,
+                  color: Colors.red,
+                ),
+              ],
+            ),
           ),
+
+          /// View Models export [body]
+          body: Center(
+              child: Column(
+            children: [
+              const Text('Splash View'),
+              ElevatedButton(
+                  onPressed: () {
+                    final snackBar = SnackBar(
+                      backgroundColor: Colors.amber,
+                      content: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Icon(Icons.favorite),
+                            const Text("FSC IBB")
+                          ]),
+                      action: SnackBarAction(
+                        label: "Undo",
+                        onPressed: () {},
+                      ),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: const Text("Snack"))
+            ],
+          )),
         );
       },
       viewModelBuilder: () => SplashViewModel(),
